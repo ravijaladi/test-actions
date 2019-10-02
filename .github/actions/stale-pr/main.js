afd6ccda93615ca7c27645e5028ac9ae2be1a2b8
Andrew Wason <rectalogic@rectalogic.com>
2019-10-02 11:13:23 -0400

    stale-pr as an action

---
+++ .github/actions/stale-pr/main.js
@@ -0,0 +1,65 @@
+const core = require('@actions/core')
+const {GitHub, context} = require('@actions/github')
+const jsdiff = require('diff')
+
+process.on('unhandledRejection', handleError)
+main().catch(handleError)
+
+
+async function main() {
+    // This action only works on pull_request synchronized events
+    if (context.payload.event_name != 'pull_request' || context.payload.action != 'synchronize') {
+        console.warn('This action requires a pull_request synchronize event');
+        return;
+    }
+    const token = core.getInput('github-token', {required: true});
+    const client = new GitHub(token)
+
+    const beforeDiff = await client.repos.compareCommits({
+        owner: context.payload.repository.owner.name,
+        repo: context.payload.repository.name,
+        base: context.payload.pull_request.base.sha,
+        head: context.payload.before,
+        mediaType: {
+            format: 'diff'
+        }
+    });
+    const afterDiff = await client.repos.compareCommits({
+        owner: context.payload.repository.owner.name,
+        repo: context.payload.repository.name,
+        base: context.payload.pull_request.base.sha,
+        head: context.payload.after,
+        mediaType: {
+            format: 'diff'
+        }
+    });
+    if (beforeDiff == afterDiff) {
+        return;
+    }
+    const diffDiff = jsdiff.createTwoFilesPatch('before-patch', 'after-patch', beforeDiff, afterDiff);
+
+    // Dismiss any approved reviews of this PR if this push introduced changes
+    const options = await client.pulls.listReviews.endpoint.merge({
+        owner: context.payload.repository.owner.name,
+        repo: context.payload.repository.name,
+        pull_number: context.payload.number
+    });
+    for await (const chunk of client.paginate.iterator(options)) {
+        for (const review of chunk.data) {
+            if (review.state == 'APPROVED') {
+                await client.pulls.dismissReview({
+                    owner: context.payload.repository.owner.name,
+                    repo: context.payload.repository.name,
+                    pull_number: context.payload.number,
+                    review_id: review.id,
+                    message: diffDiff
+                });
+            }
+        }
+    }
+}
+
+function handleError(err) {
+    console.error(err)
+    core.setFailed(err.message)
+}
